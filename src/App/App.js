af6f4dfcaed444808fb984a0e632fd32a6d275ac
FibreOptics <nacalon@hotmail.com>
2018-11-22 13:11:00 +0100

    Sign in and Appbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import List from './pages/List';
+import Auth from './pages/Auth';
+import Lobby from './pages/Lobby';
 
 class App extends Component {
   render() {
     const App = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
+          <Route exact path='/' component={Auth}/>
+          <Route path='/lobby' component={Lobby}/>
         </Switch>
       </div>
     )
@@ -21,27 +21,5 @@ class App extends Component {
     );
   }
 }
-{
-/* class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <body>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Pass:
-            <input type="text" name="pass" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        </body>
-      </div>
-    );
-  }
-} */
-}
+
 export default App;

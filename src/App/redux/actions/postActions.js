649fdf001d00e6fa8fc1d753fa8edc3a395ca2c4
FibreOptics <nacalon@hotmail.com>
2018-11-24 23:19:37 +0100

    redux added

--- src/App/redux/actions/postActions.js
+++ src/App/redux/actions/postActions.js
@@ -13,7 +13,7 @@ import {FETCH_POSTS, NEW_POST} from './types';
 
 export const fetchPosts = () => dispatch => {
     console.log('infking redux');
-    fetch('https://expressokki.herokuapp.com/api/testitem',{mode:"no-cors"})
+    fetch('https://expressokki.herokuapp.com/api/testitem')
     .then(res => res.json())
     .then(list => dispatch({
             type: FETCH_POSTS,

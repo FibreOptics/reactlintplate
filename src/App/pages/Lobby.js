41a14dd36a122f015a626558ef5a2d8563c1e079
FibreOptics <nacalon@hotmail.com>
2018-11-27 18:17:36 +0100

    postAction retry

--- src/App/pages/Lobby.js
+++ src/App/pages/Lobby.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import TopNavBar from '../../Components/TopNavbar';
+import Home from '../../Components/Home';
+
 
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../redux/actions/postActions'
 
 class Lobby extends Component {
-/*  // Initialize the state
-  constructor(props){
-    super(props);
-    this.state = {
-      list: []
-    }
-  }
-
-  // Retrieves the list of items from the Express app
-  getList = () => {
-    fetch('/api/testitem')
-    .then(res => res.json())
-    .then(list => this.setState({ list }))
-  }
-*/ 
-
+//state of chat ready to send
   componentWillMount() {
     this.props.fetchPosts();
   }
@@ -31,24 +18,17 @@ class Lobby extends Component {
     return (
       <div className="App">
         <TopNavBar/>
+        <Home/>
         <h1>List of Items</h1>
-        {list.length ? 
-        (<div>
-            {/* Render the list of items */}
-            {list.map((item,i) => {
-              return(
-                <div  key={i}>
-                  <li>
-                    {i}:{item}
-                  </li>
-                </div>
-              );
+        {list.length ? (
+          <div>{list.map((item) => {
+            return(
+              <div>{item}</div>);
             })}
-        </div>) 
-       :(<div>
-            <h2>No List Items Found</h2>
-         </div>)
-      }
+          </div>) : (
+            <div><h2>No List Items Found</h2></div>
+          )
+        }
       </div>
     );
   }
